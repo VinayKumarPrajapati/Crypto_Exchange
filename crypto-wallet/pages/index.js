545db898fd3ee4735e8b53ae79e94c8e2b85397c
VinayKumarPrajapati <107835976+VinayKumarPrajapati@users.noreply.github.com>
2022-07-17 10:07:07 +0530

    reset code for better restructer

--- crypto-wallet/pages/index.js
+++
@@ -1,8 +0,0 @@
-import Head from "next/head";
-import styled from "styled-components";
-import { useWeb3 } from "@3rdweb/hooks";
-
-export default function Home() {
-	const { address, connectWallet } = useWeb3();
-	return <div>Connect Your Wallet</div>;
-}
